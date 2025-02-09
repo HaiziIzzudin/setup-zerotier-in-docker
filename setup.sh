17c2a18b61abd4a5320fd0586fd13ec0cc667698
haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2025-02-09 12:27:52 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && apt install curl && \
+apt update && apt upgrade -y && apt install curl -y && \
 curl https://install.zerotier.com/ | bash && \
 /usr/sbin/zerotier-one -d && \
 echo "Please run \n /usr/sbin/zerotier-cli join YOUR_ZEROTIER_NETWORK"
